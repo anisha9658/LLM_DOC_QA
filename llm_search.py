c7e5b96522633e2fe6a1a62cd7a97839ef622a00
Anisha Mohanty <101242432+anisha9658@users.noreply.github.com>
2025-04-28 15:36:37 +0530

    Update llm_search.py

--- llm_search.py
+++ llm_search.py
@@ -5,7 +5,7 @@ def query_openai_api(prompt):
     llm = OpenAI(
         temperature=0.7,
         model_name="gpt-3.5-turbo-instruct",  # or text-davinci-003
-        openai_api_key="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"         # Optional if already set in ENV
+        openai_api_key=""         # Optional if already set in ENV
     )
     response = llm.predict(prompt)
     return response
